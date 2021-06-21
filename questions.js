e20f3373326bbb38add56f663b95f5c575797e1f
Oforomeh Oshomo <oshomo.oforomeh2010@gmail.com>
2021-06-21 15:24:50 +0200

    Added question

---
+++ questions.js
@@ -0,0 +1,11 @@
+export const questions = [
+  {
+    question: "You are travelling at 50km/h and have a reaction time of 1 second. What is the reaction distance according to the rule of thumb?",
+    type: "string",
+    answer: "15",
+    answer_in: "meters",
+    explanation: "Reaction distance is (speed)/10 * 3. That means at 50km/h, reaction distance is (50)/10 * 3 = 15 meters",
+  },
+];
+
+export default questions;
